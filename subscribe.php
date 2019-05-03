d1057e45024c5394105c57e5fda381c1cbee8fce
erfan kargosha <ekargosha88@gmail.com>
2019-05-03 14:27:34 +0430

    first commit

---
+++ subscribe.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Subscribe
+Plugin URI: http://erfankargosha.ir/
+Description: This is a test plugin.
+Author: Erfan Kargosha
+Version: 1.0.0
+ */
+
+defined('ABSPATH') || exit();
+//register_activation_hook( __FILE__, 'subscribe_activation' );
+//
+//function subscribe_activation() {
+//
+//}
+//
+//register_deactivation_hook( __FILE__, 'subscribe_deactivation' );
+//
+//function subscribe_deactivation(){
+//
+//}
+
